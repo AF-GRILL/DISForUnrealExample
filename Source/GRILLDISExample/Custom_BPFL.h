2ba3c9e8554e3223cadae03bd07c296c497d3a12
Quintin Oliver <oliver.quintin.l@gmail.com>
2022-06-17 11:32:37 -0400

    Added custom TMS and raster overlay functions

--- Source/GRILLDISExample/Custom_BPFL.h
+++ Source/GRILLDISExample/Custom_BPFL.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include <CesiumRuntime/Public/CesiumIonRasterOverlay.h>
+#include <CesiumRuntime/Public/CesiumTileMapServiceRasterOverlay.h>
 #include "Custom_BPFL.generated.h"
 
 /**
@@ -17,4 +19,11 @@ class GRILLDISEXAMPLE_API UCustom_BPFL : public UBlueprintFunctionLibrary
 	/** Get Custom Config Var! These are stored in Saved/Config/Windows/Game.ini */
 	UFUNCTION(BlueprintPure)
 	static FString GetCustomConfigVar_String(FString SectionName, FString VariableName, bool& IsValid);
+
+	UFUNCTION(BlueprintCallable)
+	static void SetTmsUrl(UCesiumTileMapServiceRasterOverlay* TmsServer, FString NewUrl);
+
+	UFUNCTION(BlueprintCallable)
+	static void SetRasterOverlayData(UCesiumIonRasterOverlay* IonRasterOverlay, FString NewIonAccessToken, int NewIonAssetId);
+
 };
