3b769262cc215377021ab65d566520b29db54e35
huggins <huggy2014@yahoo.com>
2025-03-17 13:00:14 -0400

    Update ini and GRILLDISExampleEditor.Target.cs to support 5.4 updates

--- Source/GRILLDISExampleEditor.Target.cs
+++ Source/GRILLDISExampleEditor.Target.cs
@@ -8,7 +8,7 @@ public class GRILLDISExampleEditorTarget : TargetRules
 	public GRILLDISExampleEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "GRILLDISExample" } );
 	}
